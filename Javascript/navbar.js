cd47d8d09fee2e572a6de4ef9ec2c18dc6f91f9f
chapman91 <chapmandoneil91@gmail.com>
2023-08-12 17:32:20 -0400

    Navbar Sticky

--- Javascript/navbar.js
+++ Javascript/navbar.js
@@ -3,12 +3,26 @@
 
 document.addEventListener("DOMContentLoaded", function() {
   // Your "navbar.js" code goes here
+
+  // Select element ID 'navbar' in the HTML DOM
   var navbar = document.getElementById('navbar');
+
+  // Calculates current vertical position
   var scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
 
+  //  
   if (scrollPosition >= 100) {
     navbar.classList.add('sticky');
-  } else {
+  } 
+  else {
     navbar.classList.remove('sticky');
   }
 });
+
+
+
+// What is the aim of your code?
+
+// Where is the navbar element located in navbar.html?
+
+// 
